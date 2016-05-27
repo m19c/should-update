3fc66a47dc1fff7c0f51817e0cb808a47b96e736
Marc Binder <marc.binder@boerse-go.de>
2016-05-27 10:31:52 +0200

    Init repo

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+import shouldUpdate from '../';
+
+describe('shouldUpdate', () => {
+  it('no changes', () => {
+    shouldUpdate(
+      ['id', 'user.name'],
+      { id: 1, user: { name: 'Jon Doe' } },
+      { id: 1, user: { name: 'Jon Doe' } }
+    ).should.be.not.ok();
+  });
+
+  it('has changes', () => {
+    shouldUpdate(
+      ['id', 'user.name'],
+      { id: 1, user: { name: 'Jon Doe' } },
+      { id: 1, user: { name: 'Lara Doe' } }
+    ).should.be.ok();
+  });
+});
